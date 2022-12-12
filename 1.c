b5d02b0be18d480f8415db441bbdeb0fe973b471
ripgamer <54296032+ripgamer@users.noreply.github.com>
2022-12-12 17:41:19 +0530

    1

---
+++ 1.c
@@ -0,0 +1,21 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int mk1,mk2,mk3,mk4,mk5,per,avg,tot;
+    printf("enter marks of subject 1 :");
+    scanf("%d/n",&mk1);
+    printf("enter marks of subject 1 :");
+    scanf("%d",&mk2);
+     printf("enter marks of subject 1 :");
+    scanf("%d",&mk3);
+     printf("enter marks of subject 1 :");
+    scanf("%d",&mk4);
+     printf("enter marks of subject 1 :");
+    scanf("%d",&mk5);
+    sum=mk1+mk2+mk3+mk4+mk5;
+    
+    return 0;
+}
