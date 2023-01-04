4a2f4f743cf072795e002b125d0725ce552f7a4f
ripgamer <54296032+ripgamer@users.noreply.github.com>
2023-01-04 12:23:56 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int mk1,mk2,mk3,mk4,mk5,max;
-    int tot=mk1+mk2+mk3+mk4+mk5;
-    int avg=(mk1+mk2+mk3+mk4+mk5)/5;
-    int per=(tot/max)*100;
-     printf("enter marks of subject 1 :");
-    scanf("%d/n",&mk1);
-     printf("enter marks of subject 2 :");
-    scanf("%d",&mk2);
-     printf("enter marks of subject 3 :");
-    scanf("%d",&mk3);
-     printf("enter marks of subject 4 :");
-    scanf("%d",&mk4);
-     printf("enter marks of subject 5 :");
-    scanf("%d",&mk5);
-    printf("enter maximum marks obatainble marks :");
-    scanf("%d",&m);
-    printf(" percentage of the student is :%d \n Average score is :%d \n"per,avg);    
+    int mk1,mk2,mk3,mk4,mk5;
+    printf("enter marks of phy,maths,chem,english,cp:");
+    scanf("%d %d %d %d %d",&mk1,&mk2,&mk3,&mk4,&mk5);
+    printf(" percentage of the student is :%d \n Average score is :%d \n",((mk1+mk2+mk3+mk4+mk5)*100)/500,(mk1+mk2+mk3,mk4,mk5)/5);    
     return 0;
 }
