e8273d0909f97265a8c98644b7bdbf8b86280657
ripgamer <54296032+ripgamer@users.noreply.github.com>
2023-01-13 15:59:50 +0530

    v 1.0

---
+++ n_natural_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=1,n;
+    printf("\nEnter nth number till you want to display:\n");
+    while(i<=n)
+    {
+        printf("natural number till %d is : \n %d",n,i);
+        i++;
+    }
+}
