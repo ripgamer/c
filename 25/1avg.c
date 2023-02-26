b35e98761a87c15114789e0e7f6636e47b68bec4
ripgamer <54296032+ripgamer@users.noreply.github.com>
2023-02-26 19:00:58 +0000

    25

--- 25/1avg.c
+++ 25/1avg.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int;
+    int i,mk1,mk2,mk3,mk4,mk5;
+    for(i=1;i<=25;i++)
+    {
     
-    printf(enter)
+    printf("enter marks of student %d phy,maths,chem,english,cp:",i);
+    scanf("%d %d %d %d %d",&mk1,&mk2,&mk3,&mk4,&mk5);
+    printf(" percentage of the student is :%d \n Average score is :%d \n",((mk1+mk2+mk3+mk4+mk5)*100)/500,(mk1+mk2+mk3,mk4,mk5)/5);    
+    
+    }
+    return 0;
 }
